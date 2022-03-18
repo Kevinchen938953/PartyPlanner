cf198663c2eb5e8a90a440f802fca9226ae6f5db
Kevinchen938953 <59925517+Kevinchen938953@users.noreply.github.com>
2022-03-17 23:18:15 -0400

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,37 +1,183 @@
 import java.util.Scanner;
-//sort into ten groups where no more than two people from same company sit together
-//create table class
-//create table class
-//create collection of table objects that contain people objects
-public class People
-{
-	private int pID;
-	private int tableID;
-	private int companyID;
-	private String name;
-	private String company;
-	public People(int pID, String name, int companyID)//Constructer to create Person objects with these attributes
-	{
-		this.name = name;
-		this.pID = pID;
-		this.companyID = companyID;
-	}
-}
+import java.io.*;
+import java.util.ArrayList;
 
-/*public class Table
+
+
+public class Tester
 {
-	int tableID;
-	Person[10] = new Person[10];
-	public Table(int tableID)
-	{
 	
-	}
-}
 
-//suppose
-public class Banquet
-{
-	int numPPL;
-	int[][] pplFrCo= int[16][20];//companyID, num from company there are sixteen companies with 20 people at each company
-	Person[][] bt = new Person[10][10];
-*/
+	public static void main(String[] args) throws IOException
+	{
+			int random = (int)(Math.random()*99);
+		//String p="";
+		String row;
+		Integer[][]tables = new Integer[10][10];
+		//ArrayList<ArrayList<People>>tables = new ArrayList<ArrayList<People>>();
+		ArrayList<People> partylist = new ArrayList<People>();
+		
+		
+			BufferedReader csvReader = new BufferedReader(new FileReader("names.csv"));
+			while ((row = csvReader.readLine()) != null) 
+			{
+				String[] data = row.split(",");
+				System.out.println(data[3]);
+				Integer.parseInt(data[3]);
+				Integer.parseInt(data[0]);
+				People p1 = new People(Integer.parseInt(data[0]), data[1], Integer.parseInt(data[3]));
+				partylist.add(p1);
+				//System.out.println(p1.toString());
+				
+				
+				
+				
+			}
+			csvReader.close();
+		
+			    partylist.add(new People(1, "Kevin", 15));
+				partylist.add(new People(2, "Phillip", 5));
+				partylist.add(new People(3, "Dave", 4));
+				partylist.add(new People(4, "Jim", 3));
+				partylist.add(new People(5, "David", 2));
+				partylist.add(new People(6, "Jaden ", 3));
+				partylist.add(new People(7, "Kaden", 5));
+				partylist.add(new People(8, "Jerome", 6));//manually add people to partylist
+				partylist.add(new People(9, "Jake", 7));
+				partylist.add(new People(10, "Arnie", 8));
+				System.out.print(partylist.size());
+				int count = 0;
+				int onecount=0;
+				int twocount=0;
+				int threecount=0;
+				int fourcount=0;
+				int fivecount=0;
+				int sixcount=0;
+				int sevencount=0;
+				int eightcount=0;
+				int ninecount=0;
+				int tencount=0;
+				int elevencount=0;
+				int twelvecount=0;
+				int thirteencount=0;
+				int fourteencount=0;
+				int fifteencount=0;
+			
+			
+				for(int x=0; x<10; x++)
+				{
+					for(int y=0; y<10; y++)
+					{
+						tables[y][x] = partylist.get(count).companyID;
+						count++;
+						
+					}
+					
+				}
+				//after populating for the first time, check every table to see if there are more than 2. For every company person present, there will be a counter that counts the amount
+				for(int x=0; x<10; x++)
+				{
+					for(int y=0; y<10; y++)
+					{
+						if(tables[x][y]==1)
+						{
+							onecount++;
+						}
+						else if(tables[x][y]==2)
+						{
+							twocount++;
+						}
+						else if(tables[x][y]==3)
+						{
+							threecount++;
+						}
+						else if(tables[x][y]==4)
+						{
+							fourcount++;
+						}
+						else if(tables[x][y]==5)
+						{
+							fivecount++;
+						}
+						else if(tables[x][y]==6)
+						{
+							sixcount++;
+						}
+						else if(tables[x][y]==7)
+						{
+							sevencount++;
+						}
+						else if(tables[x][y]==8)
+						{
+							eightcount++;
+						}
+						else if(tables[x][y]==9)
+						{
+							ninecount++;
+						}
+						else if(tables[x][y]==10)
+						{
+							tencount++;
+						}
+						else if(tables[x][y]==11)
+						{
+							elevencount++;
+						}
+						else if(tables[x][y]==12)
+						{
+							twelvecount++;
+						}
+						else if(tables[x][y]==13)
+						{
+							thirteencount++;
+						}else if(tables[x][y]==14)
+						{
+							fourteencount++;
+						}else if(tables[x][y]==15)
+						{
+							fifteencount++;
+						}
+						
+					}
+					
+				}
+			//random number generates from 0-99
+				while(onecount>2 || twocount>2 || threecount>2 || fourcount>2 || fivecount>2 || sixcount>2 || sevencount>2 || eightcount>2 || ninecount>2 || tencount>2 || elevencount>2 || twelvecount>2 || thirteencount>2 ||fourteencount>2 || fifteencount>2)
+				{
+					for(int i=0; i<10; i++)
+					{
+						for(int n=0; n<10; n++)
+						{
+							tables[n][i] = partylist.get(random).companyID;//continuosly randomly populate 2d array with arraylist items as long as number of people in same company exceed 2
+						}
+					}
+				}
+				
+				
+				
+					
+				}
+				//System.out.println(samecount);
+			
+		
+		
+			
+		
+		
+		//Create 2d arraylist to represent rows of tables each with 10 people
+		
+		
+		// ArrayList <People> party = new ArrayList<People>();
+		//Import name.csv file into the code
+		//Define each dataset as a people object
+		//use for each loop to populate the ArrayList
+		
+		
+		
+	    
+		
+		
+			
+		
+	}
+
